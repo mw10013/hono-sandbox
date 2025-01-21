ed909f1031c39dbae4e0f1b7b24542810972c10d
Michael Wu <mw10013@gmail.com>
2025-01-21 15:34:17 -0500

    additional route

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,31 @@
-import { issuer } from "@openauthjs/openauth"
-import { CloudflareStorage } from "@openauthjs/openauth/storage/cloudflare"
+import { issuer } from "@openauthjs/openauth";
+import { CloudflareStorage } from "@openauthjs/openauth/storage/cloudflare";
 import {
   type ExecutionContext,
   type KVNamespace,
-} from "@cloudflare/workers-types"
-import { subjects } from "./subjects.js"
-import { PasswordProvider } from "@openauthjs/openauth/provider/password"
-import { PasswordUI } from "@openauthjs/openauth/ui/password"
+} from "@cloudflare/workers-types";
+import { subjects } from "./subjects.js";
+import { PasswordProvider } from "@openauthjs/openauth/provider/password";
+import { PasswordUI } from "@openauthjs/openauth/ui/password";
+import { Hono } from "hono";
 
 interface Env {
-  CloudflareAuthKV: KVNamespace
+  CloudflareAuthKV: KVNamespace;
 }
 
 async function getUser(email: string) {
   // Get user from database
   // Return user ID
-  return "123"
+  return "123";
 }
 
 export default {
   async fetch(request: Request, env: Env, ctx: ExecutionContext) {
-    return issuer({
+    // Impossible to extend with additional routes:https://github.com/openauthjs/openauth/issues/127#issuecomment-2569976202
+    const app = new Hono();
+    app.get("/health", (c) => c.json({ status: "ok" }));
+
+    const openauth = issuer({
       storage: CloudflareStorage({
         namespace: env.CloudflareAuthKV,
       }),
@@ -29,19 +34,24 @@ export default {
         password: PasswordProvider(
           PasswordUI({
             sendCode: async (email, code) => {
-              console.log(email, code)
+              console.log(email, code);
             },
-          }),
+          })
         ),
       },
       success: async (ctx, value) => {
         if (value.provider === "password") {
           return ctx.subject("user", {
             id: await getUser(value.email),
-          })
+          });
         }
-        throw new Error("Invalid provider")
+        throw new Error("Invalid provider");
       },
-    }).fetch(request, env, ctx)
+    });
+
+    // Mount OpenAuth routes last
+    app.route("/", openauth);
+
+    return app.fetch(request, env, ctx);
   },
-}
+};
